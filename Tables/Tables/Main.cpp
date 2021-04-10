46803484b12de7c8bd63456540bec71862a57124
Дмитрий Розанов <rozandima3@gmail.com>
2021-04-11 00:36:54 +0300

    beginning

---
+++ Tables/Tables/Main.cpp
@@ -0,0 +1,15 @@
+#include "iostream"
+
+#include "Polinom.h"
+#include "Unordered_massive.h"
+#include "AVL_tree.h"
+#include "Hash_table.h"
+
+using namespace std;
+
+int main()
+{
+
+}
+
+
---
+++ Tables/include/unordered_massive.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "iostream"
+
+#include "Polinom.h"
+
+using namespace std;
+
+class unordered_massive
+{
+
+};
